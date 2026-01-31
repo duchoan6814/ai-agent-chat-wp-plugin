a6ad25bf083bf3652bc8a88c1ad9ac09d8163b02
Hoan Truong <hoantd@olabs.net>
2026-01-31 14:31:50 +0700

    setting color with var

--- ai-chat.php
+++ ai-chat.php
@@ -34,8 +34,9 @@ function ai_chat_render_html()
 add_action('wp_enqueue_scripts', 'ai_chat_enqueue_assets');
 function ai_chat_enqueue_assets()
 {
-    wp_enqueue_style('ai-chat-style', plugins_url('assets/css/style.css', __FILE__));
-    wp_enqueue_style('ai-chat-chat-box-style', plugins_url('assets/css/chat-box.css', __FILE__));
+    wp_enqueue_style('ai-chat-vars', plugins_url('assets/css/variables.css', __FILE__));
+    wp_enqueue_style('ai-chat-style', plugins_url('assets/css/style.css', __FILE__), array('ai-chat-vars'));
+    wp_enqueue_style('ai-chat-chat-box-style', plugins_url('assets/css/chat-box.css', __FILE__), array('ai-chat-vars'));
     wp_enqueue_script('ai-chat-script', plugins_url('assets/scripts/script.js', __FILE__), array('jquery'), '1.0', true);
     wp_enqueue_script('ai-chat-chat-box-script', plugins_url('assets/scripts/chat-box.js', __FILE__), array('jquery'), '1.0', true);
 
